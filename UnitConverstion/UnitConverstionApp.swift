b9138eb2be0e1a9b3bb0406ec0d7d460dbec9a53
Trung Nguyen <nhattrungnguyen14@yahoo.com>
2023-10-11 11:24:59 -0400

    Initial Commit

---
+++ UnitConverstion/UnitConverstionApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverstionApp.swift
+//  UnitConverstion
+//
+//  Created by Trung Nguyen on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverstionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
